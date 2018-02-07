1ec873db4624b684098671f2b29b71ab3f735bc3
nilukagun <nilukagun@sss2.c.fluted-harmony-184422.internal>
2018-02-08 04:26:05 +0530

    App Health Email Notification Added

--- src/main/java/com/sss/wc/controllers/CommonController.java
+++ src/main/java/com/sss/wc/controllers/CommonController.java
@@ -30,8 +30,7 @@ import javax.mail.internet.MimeMessage;
 @SessionScoped
 public class CommonController implements Serializable {
 
-    @EJB
-    WebUserFacade webUserFacade;
+   
 
     /**
      * Creates a new instance of LanguageController
@@ -78,74 +77,6 @@ public class CommonController implements Serializable {
 
     
     
-    
-     @SuppressWarnings("unused")
-//    @Schedule(minute = "1", second = "1", dayOfMonth = "*", month = "*", year = "*", hour = "1", persistent = false)
-    @Schedule(minute = "59", second = "59", dayOfMonth = "*", month = "*", year = "*", hour = "*", persistent = false)
-//    @Schedule(minute = "59", second = "59", hour = "23", dayOfMonth = "Last", info = "2nd Scheduled Timer", persistent = false)
-//    @Schedule(second="*/1", minute="*",hour="*", persistent=false)
-    public void myTimer() {
-        long userCount = 0;
-        String errorMessage;
-        try {
-            userCount = webUserFacade.count();
-            sendEmail1("sunila.soft@gmail.com",  "SE is OK", "SE is OK");
-        } catch (Exception e) {
-            errorMessage = e.getMessage();
-            sendEmail1("Error in SE", errorMessage);
-        }
-        System.out.println("userCount = " + userCount);
-    }
-
-    public void sendEmail1(String messageHeading, String messageBody) {
-        sendEmail1("buddhika.ari@gmail.com", messageHeading, messageBody, "lakmedipro@gmail.com", "Bud7NilG");
-    }
-
-    public void sendEmail1(String toEmail, String messageHeading, String messageBody) {
-        sendEmail1(toEmail, messageHeading, messageBody, "lakmedipro@gmail.com", "Bud7NilG");
-    }
-
-    public void sendEmail1(String toEmail, String messageHeading, String messageBody, String fromUserName, String fromPassword) {
-        final String username = fromUserName;
-        final String password = fromPassword;
-        Properties props = new Properties();
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.host", "smtp.gmail.com");
-        props.put("mail.smtp.port", "587");
-        Session session = Session.getInstance(props,
-                new javax.mail.Authenticator() {
-            protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication(username, password);
-            }
-        });
-        try {
-            Message message = new MimeMessage(session);
-            message.setFrom(new InternetAddress(fromUserName));
-            message.setRecipients(Message.RecipientType.TO,
-                    InternetAddress.parse(toEmail));
-            message.setSubject(messageHeading);
-            message.setText(messageBody);
-//            BodyPart msbp1 = new MimeBodyPart();
-//            msbp1.setText("Final Lab report of patient");
-
-//            MimeBodyPart msbp2 = new MimeBodyPart();
-//            DataSource source = new FileDataSource("LabReport.pdf");
-//            msbp2.setDataHandler(new DataHandler(source));
-//            msbp2.setFileName("/Labreport.pdf");
-//            Multipart multipart = new MimeMultipart();
-//            multipart.addBodyPart(msbp1);
-//            multipart.addBodyPart(msbp2);
-//            message.setContent(multipart);
-            Transport.send(message);
-
-            System.out.println("Send Successfully");
-
-        } catch (MessagingException e) {
-            throw new RuntimeException(e);
-        }
-
-    }
     
     
 }
--- target/generated-sources/annotations/com/sss/wc/entity/Bill_.java
+++ target/generated-sources/annotations/com/sss/wc/entity/Bill_.java
@@ -14,7 +14,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-02-08T03:39:52")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-02-08T04:25:44")
 @StaticMetamodel(Bill.class)
 public class Bill_ { 
 
