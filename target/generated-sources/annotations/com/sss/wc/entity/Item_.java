281024826083c66f2ee1f35c8f5e33920251e7e3
buddhika <buddhika@weblk.c.united-idea-214200.internal>
2020-08-03 19:25:41 +0000

    Merge origin/master
    Conflicts:
    	target/classes/com/sss/wc/controllers/WebUserController$WebUserControllerConverter.class
    	target/classes/com/sss/wc/controllers/WebUserController.class
    	target/generated-sources/annotations/com/sss/wc/entity/Item_.java
    	target/generated-sources/annotations/com/sss/wc/entity/ProgramPreferences_.java
    	target/generated-sources/annotations/com/sss/wc/entity/UserPrivilege_.java
    	target/generated-sources/annotations/com/sss/wc/entity/WebUser_.java
    	target/maven-archiver/pom.properties

--- target/generated-sources/annotations/com/sss/wc/entity/Item_.java
+++ target/generated-sources/annotations/com/sss/wc/entity/Item_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-02-29T19:01:45")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-08-03T19:25:01")
 @StaticMetamodel(Item.class)
 public class Item_ { 
 
--- target/generated-sources/annotations/com/sss/wc/entity/UserPrivilege_.java
+++ target/generated-sources/annotations/com/sss/wc/entity/UserPrivilege_.java
@@ -6,7 +6,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-02-29T19:01:45")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-08-03T19:25:01")
 @StaticMetamodel(UserPrivilege.class)
 public class UserPrivilege_ { 
 
