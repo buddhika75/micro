b1d5e311b94c6bf07ff091c2d666753f40971707
buddhika <buddhika@weblk.c.united-idea-214200.internal>
2020-08-05 23:09:55 +0000

    SSH Migrated

--- target/generated-sources/annotations/com/sss/wc/entity/Item_.java
+++ target/generated-sources/annotations/com/sss/wc/entity/Item_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-08-03T19:25:01")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-08-03T19:49:51")
 @StaticMetamodel(Item.class)
 public class Item_ { 
 
